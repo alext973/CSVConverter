b318804b0c484a68d1e55f60f286a7e2acc5e9ca
alext973 <alext9753@web.de>
2025-05-04 10:37:35 +0200

    Update

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from app import start_app
 from updater import check_for_updates
 
 if __name__ == "__main__":
-    check_for_updates()  # Check for updates when the program starts
+    check_for_updates()
     start_app()
